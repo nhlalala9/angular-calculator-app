6bca65b70c431a98ce1499ca0e5761dc958a53eb
Nhlalala <nhlalala60@gmail.com>
2023-05-17 11:59:31 +0200

    if statement

--- Calculator/src/app/calculator/calculator.component.ts
+++ Calculator/src/app/calculator/calculator.component.ts
@@ -7,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  input:string='';
-  total:string='';
+  input: string = '';
+  total: string = '';
+
+  totalNum(num: string) {
+    //  not allow more than once
+    if (num == ".") {
+      if (this.input != "") {
+        const lastNum = this.getLastOperand()
+        console.log(lastNum.lastIndexOf("."))
+        if (lastNum.lastIndexOf(".") >= 0)
+          return;
+      }
+    }
+
+
+
+  }
+
+  getLastOperand() {
+    let pos: number;
+    console.log(this.input)
+    pos = this.input.toString().lastIndexOf("+")
+    if (this.input.toString().lastIndexOf("-") > pos) pos = this.input.lastIndexOf("-")
+    if (this.input.toString().lastIndexOf("*") > pos) pos = this.input.lastIndexOf("*")
+    if (this.input.toString().lastIndexOf("/") > pos) pos = this.input.lastIndexOf("/")
+    console.log('Last ' + this.input.substr(pos + 1))
+    return this.input.substr(pos + 1)
+  }
+
 
   constructor() { }
 

e56fb1ff1868ee570c7217b1c05c3ddea2725e1d
Nhlalala <nhlalala60@gmail.com>
2023-05-17 11:51:38 +0200

    input

--- Calculator/src/app/calculator/calculator.component.ts
+++ Calculator/src/app/calculator/calculator.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   input:string='';
-  result:string='';
+  total:string='';
 
   constructor() { }
 
